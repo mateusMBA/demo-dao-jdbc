95e47a21f942565d1a0ae763fcd599862aebde1c
Mateus Almeida <mateus.barros.almeida@gmail.com>
2024-07-23 16:15:20 -0300

    delete implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -35,5 +38,13 @@ public class Program {
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Update Completed");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete Completed");
+
+        sc.close();
     }
 }
