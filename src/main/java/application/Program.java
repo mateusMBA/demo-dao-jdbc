c6f1119943cd20a1061ad8d1f46dc690cc03e60b
Mateus Almeida <mateus.barros.almeida@gmail.com>
2024-07-23 15:38:19 -0300

    insert implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -17,12 +17,17 @@ public class Program {
         System.out.println(seller);
 
         System.out.println("\n=== TEST 2: seller findByDepartment ===");
-        Department dep = new Department(2,null);
-        List<Seller> sellers = sellerDao.findByDepartment(dep);
+        Department department = new Department(2,null);
+        List<Seller> sellers = sellerDao.findByDepartment(department);
         sellers.forEach(System.out::println);
 
         System.out.println("\n=== TEST 3: seller findAll ===");
         sellers = sellerDao.findAll();
         sellers.forEach(System.out::println);
+
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
